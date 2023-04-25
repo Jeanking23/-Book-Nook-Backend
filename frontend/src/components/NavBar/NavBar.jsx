508ea41374e95fbd10141ee54ce106d7a9ea3f70
Jeanking23 <jeandedieu405@gmail.com>
2023-04-25 09:52:08 -0400

    new

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -5,26 +5,44 @@ import AuthContext from "../../context/AuthContext";
 import "./NavBar.css";
 
 const Navbar = () => {
-  const { logoutUser, user } = useContext(AuthContext);
-  const navigate = useNavigate();
-  return (
-    <div className="navBar">
-      <ul>
-        <li className="brand">
-          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-            <b>React/Flask JWT</b>
-          </Link>
-        </li>
-        <li>
-          {user ? (
-            <button onClick={logoutUser}>Logout</button>
-          ) : (
-            <button onClick={() => navigate("/login")}>Login</button>
-          )}
-        </li>
-      </ul>
-    </div>
-  );
+    const { logoutUser, user } = useContext(AuthContext);
+    const navigate = useNavigate();
+    return ( <
+        div className = "navBar" >
+        <
+        ul >
+        <
+        li className = "brand" >
+        <
+        Link to = "/"
+        style = {
+            { textDecoration: "none", color: "white" }
+        } >
+        <
+        b > React / Flask JWT < /b> < /
+        Link > <
+        /li> <
+        li > {
+            user ? ( <
+                button onClick = { logoutUser } > Logout < /button>
+            ) : ( <
+                button onClick = {
+                    () => navigate("/login")
+                }
+                className = "btm" > Login < /button>
+
+            )
+        } <
+        /li> <
+        li >
+        <
+        button onClick = {
+            () => navigate("/Search")
+        } > Search < /button> < /
+        li > <
+        /ul> < /
+        div >
+    );
 };
 
 export default Navbar;
